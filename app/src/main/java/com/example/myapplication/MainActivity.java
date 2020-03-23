056212feeee9af52decbd9bf7ab78f9a6e07fb84
yohan <yohan.madeira@gmail.com>
2020-03-23 10:03:31 +0100

    Add first exercice

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Exercice 1
+        SensorManager sensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> liste = sensorManager.getSensorList(Sensor.TYPE_ALL);
+        for (int i = 0 ; i < liste.size() ; i++)
+            Log.d("Mes capteurs" , liste.get(i).toString());
+
+        //Exercice 2
+
+
+
     }
 }
